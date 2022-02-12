31e764644668f114a4416b7c1be237536e8fbe01
Sofi <soshapancake@mail.ru>
2022-02-12 17:42:53 +0300

    Выполнение ДЗ №5 задание 5

--- Guseva_Sofia_dz_5/task_5_5.py
+++ Guseva_Sofia_dz_5/task_5_5.py
@@ -6,7 +6,7 @@ result = [23, 1, 3, 10, 4, 11]
 Подсказка: напишите сначала решение «в лоб». Потом подумайте об оптимизации."""
 
 
-
+#Попытка решить по-другому
 def get_uniq_numbers(src: list):
     for el in src:
         if src.count(el) == 1:
@@ -15,3 +15,21 @@ def get_uniq_numbers(src: list):
 
 src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
 print(*get_uniq_numbers(src))
+
+#Решение как на уроке
+
+"""
+def get_uniq_numbers(src: list):
+    uniq_nums = set()
+    tmp = set()
+    for num in src:
+        if num not in tmp:
+            uniq_nums.add(num)
+        else:
+            uniq_nums.discard(num)
+        tmp.add(num)
+    return [num for num in src if num in uniq_nums]
+
+src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+print(*get_uniq_numbers(src))
+"""
