9138f039af5f0a27ad310c90896fff4bca53813a
Sofi <soshapancake@mail.ru>
2022-02-12 14:21:57 +0300

    Выполнение ДЗ №5 задание 5

--- Guseva_Sofia_dz_5/task_5_5.py
+++ Guseva_Sofia_dz_5/task_5_5.py
@@ -8,19 +8,9 @@ result = [23, 1, 3, 10, 4, 11]
 
 
 def get_uniq_numbers(src: list):
-    uniq_nums = set()
-    tmp = set()
     for el in src:
-        if el not in tmp:
-            uniq_nums.add(el)
-        else:
-            uniq_nums.discard(el)
-        tmp.add(el)
-    print(uniq_nums)
-
-    uniq_nums_ord = [el for el in src if el in uniq_nums]
-    yield uniq_nums_ord
-
+        if src.count(el) == 1:
+            yield el
 
 
 src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
